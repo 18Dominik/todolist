711f9d7c9d62f4c3f3c1b9787c84cad0b3f750af
Dominik <dominiksteffan92@gmail.com>
2024-02-13 19:17:24 +0000

    default mongoose.connect

--- todo-app/server.js
+++ todo-app/server.js
@@ -17,7 +17,7 @@ app.use(express.static('./'));
 app.use(express.json()); // for parsing application/json
 
 // Connect to MongoDB
-mongoose.connect('mongodb://mymongo:27017/todolist') //change from "localhost" to e.g. mymongo if serve.js is containerized and vice versa
+mongoose.connect('mongodb://localhost:27017/todolist') //change from "localhost" to e.g. mymongo if serve.js is containerized and vice versa
 .then(() => console.log('Successfully connected to MongoDB.'))
 .catch((error) => console.error('Error connecting to MongoDB:', error));
 
