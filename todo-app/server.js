48068172819ef29b96ddbf248a24d8bdc9e97ee9
Dominik <dominiksteffan92@gmail.com>
2024-02-13 19:31:51 +0000

    it works - with better comments :)

--- todo-app/server.js
+++ todo-app/server.js
@@ -76,7 +76,9 @@ curl -X POST http://localhost:3000/todolist \
 
 curl -H "Accept: application/json" http://localhost:3000/todolist
 
-Run application
+Run application on local host
+Make sure you change mymongo to localhost: mongoose.connect('mongodb://localhost:27017/todolist') instead of mongoose.connect('mongodb://mymongo:27017/todolist')
+
 -> in working directory ./todo-app: node server.js
 
 Start Mongo DB Instance
@@ -85,12 +87,14 @@ Start Mongo DB Instance
 3. Connect with VSCode extension: Connection string to mongo db VS Code extension with default docker settings: mongodb://localhost:27017
 
 Containerize Application
+Make sure you change localhost to mymongo: mongoose.connect('mongodb://mymongo:27017/todolist') instead of mongoose.connect('mongodb://localhost:27017/todolist')
 1.create network: docker network create mynetwork
 2. Run MongoDB in network:  docker run --name mymongo --network mynetwork -d -p 27017:27017 mongo
 3. Create image: docker build -t todoapp .
 4. Run application container image: docker run --network mynetwork -p 3000:3000 todoapp
 
 Docker Compose
+Make sure you change localhost to mymongo: mongoose.connect('mongodb://mymongo:27017/todolist') instead of mongoose.connect('mongodb://localhost:27017/todolist')
 Start: docker-compose up --build
 Close: docker-compose down
 
